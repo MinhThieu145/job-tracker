265ec8d7678bee7a67d8e6ba5d1cdf285d9d9678
MinhThieu145 <nguyen864@usf.edu>
2026-05-01 13:17:41 +0700

    feat: add resume parsing capability and isGolden toggle to the new resume page

--- app/resumes/new/page.tsx
+++ app/resumes/new/page.tsx
@@ -3,6 +3,10 @@
 import { Input } from "@/components/ui/input";
 import { uploadResumeToBucket } from "@/lib/utils";
 import { useState } from "react"
+import { Origami } from "lucide-react";
+import { Toggle } from "@/components/ui/toggle";
+
+import type { TextItem, TextMarkedContent } from "pdfjs-dist/types/src/display/api";
 
 
 const Page = () => {
@@ -10,20 +14,96 @@ const Page = () => {
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const [resumeLabel, setResumeLabel] = useState("");
     const [resumeNote, setResumeNote] = useState("");
+    const [resumeIsGolden, setResumeIsGolden] = useState(false);
+    const [resumeStructuredData, setresumeStructuredData] = useState(null)
 
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        // e.target.files is a FileList, which is array-like
-        // We use the optional chaining (?.) and [0] to get the first file
-        const file = e.target.files?.[0] || null;
-        setSelectedFile(file);
+    const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        console.log("file change event: ", e);
+
+        try {
+        
+            // e.target.files is a FileList, which is array-like
+            // We use the optional chaining (?.) and [0] to get the first file
+            const file = e.target.files?.[0] || null;
+            setSelectedFile(file);
+
+            // 2. convert the pdf to text
+            const convertPDFToText = async (pdfFile: File | null) => {
+                
+                // for the pdf parser
+                const pdfjs = await import('pdfjs-dist')
+                pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+
+                if (pdfFile) {
+                    // 2.1 Convert File to ArrayBuffer for PDF.js
+                    const arrayBuffer = await pdfFile?.arrayBuffer()
+
+                    // 2.2 Load the PDF 
+                    const loadingTask = pdfjs.getDocument({ data: arrayBuffer });
+                    const pdf = await loadingTask.promise;
+
+                    let fullText = "";
+
+                    // 2.3 Loop through each page
+                    for (let i = 1; i <= pdf.numPages; i++) {
+                        const page = await pdf.getPage(i)
+
+                        // get the content of the page
+                        const textContentOfPage = await page.getTextContent();
+                        console.log("text content of an pdf page (log just to see how it looks like): ", textContentOfPage);
+
+                        // we get the content of each item (pls see the log to see how the text content actually looks like )
+                        const pageText = textContentOfPage.items
+                            .map((item: TextItem | TextMarkedContent) => {
+                                if ("str" in item) {
+                                    return item.str
+                                } else{
+                                    return ""
+                                }
+                            })
+                            .join(" ")
+
+                        fullText += pageText + "\n\n"
+                    }
+                    
+                    return fullText
+                }
+            }
+            
+
+            const fullPdfText = await convertPDFToText(file)
+            
+            // 3. parsed the text with Anthropic into structured JSON
+            const response = await fetch ("/api/parse-resume/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    resumeText: fullPdfText
+                })
+            })
+
+            const resumeStructuredData = await response.json()
+
+            setresumeStructuredData(resumeStructuredData)
+
+            console.log("the structured data parsed from the resume: ", resumeStructuredData)
+
+        } catch (error) {
+            console.error("Error while parsing resume content", error);
+        }
+
     };
 
     const handleResumeSubmit = async () => {
         if (!selectedFile || !resumeLabel) {
             alert("Please upload a resume and provide a label before saving.");
             return
-        } 
-        
+        }
+
 
         // now we save the resume info to supabase database
         try {
@@ -39,14 +119,16 @@ const Page = () => {
                     label: resumeLabel,
                     notes: resumeNote,
                     fileName: fileName,
-                    fileUrl: fileUrl
+                    fileUrl: fileUrl,
+                    isGolden: resumeIsGolden,
+                    structuredData: resumeStructuredData
                 })
             });
 
             if (!response.ok) {
                 throw new Error("API request failed to save resume to db");
             }
-        
+
         } catch (error) {
             console.error("Error with upload resume:", error);
         }
@@ -98,8 +180,36 @@ const Page = () => {
                         onChange={(e) => setResumeNote(e.target.value)}
                         className="w-full p-4 rounded-lg text-sm border border-gray-400 text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
-                
+
                 </div>
+
+                {/* Toggle for the isGolden shadcn toggle*/}
+                <div className="mb-6 ">
+                    <label htmlFor="isGolden" className="block text-sm font-medium text-gray-700 mb-1">
+                        Mark as Golden Resume
+                    </label>
+                    <Toggle
+                        id="isGolden"
+                        pressed={resumeIsGolden}
+                        variant="outline"
+                        onPressedChange={(pressed) => setResumeIsGolden(pressed)}
+                    >
+
+                        <Origami className="group-data-[state=on]/toggle:fill-foreground" size={16} />
+                        {resumeIsGolden ? "Yes, this is a golden resume" : "No, it's not a golden resume"}
+                    </Toggle>
+                </div>
+
+                {/* Json display the structuredData */}
+                {resumeStructuredData && (
+                    <div className="mb-6">
+                        <h2 className="text-2xl font-bold mb-4">Parsed Resume Data (Structured JSON):</h2>
+                        <pre className="bg-[#1e1e1e] text-slate-400 p-4 rounded-md border-l-4 border-emerald-500 selection:bg-emerald-500/30 overflow-x-auto">
+                            {JSON.stringify(resumeStructuredData, null, 2)}
+                        </pre>
+                    </div>
+                )}
+
 
                 <button
                     className="w-fit self-end bg-blue-500 hover:bg-blue-600 cursor-pointer text-white text-sm font-bold py-2 px-4 rounded-lg"
